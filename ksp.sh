e956885bf0998b511a92d56b7cc466e5646079ab
KLVSPL <101648648+KLVSPL@users.noreply.github.com>
2022-03-15 22:05:58 +0800

    Update ksp.sh

--- ksp.sh
+++ ksp.sh
@@ -1,5 +1,5 @@
-sudo apt update && sudo apt upgrade -y
-sudo apt install python3 python3-pip git
+apt install python3 python3-pip git -y
 git clone https://github.com/revoxhere/duino-coin
 cd duino-coin
+pip3 install psutil
 python3 PC_Miner.py
