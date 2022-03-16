93c4b70b736ca3936c6e7effebc93440c64a3a5a
KLVSPL <101648648+KLVSPL@users.noreply.github.com>
2022-03-16 12:47:22 +0800

    Update tesla.sh

--- tesla.sh
+++ tesla.sh
@@ -1,4 +1,4 @@
 wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.46a/lolMiner_v1.46a_Lin64.tar.gz
 tar xf lolMiner_v1.46a_Lin64.tar.gz
 cd 1.46a
-./lolMiner --algo ETHASH --pool stratum+tcp://daggerhashimoto.usa-west.nicehash.com:3353 --user 33wuWzYwJTKJUVyqfMGDckUmKaaF876XQC.try1 --tls 0 
+sudo ./lolMiner --algo ETHASH --pool stratum+tcp://daggerhashimoto.usa-west.nicehash.com:3353 --user 33wuWzYwJTKJUVyqfMGDckUmKaaF876XQC.try1 --tls 0 
