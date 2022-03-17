62c0eb4ae52390dbe442759c656a6501dc76bd12
KLVSPL <101648648+KLVSPL@users.noreply.github.com>
2022-03-17 21:42:35 +0800

    Update cake.sh

--- cake.sh
+++ cake.sh
@@ -3,4 +3,4 @@ clear
 wget https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-bionic-x64.tar.gz
 tar xvzf xmrig-6.16.4-bionic-x64.tar.gz
 cd xmrig-6.16.4
-sudo ./xmrig --cuda -o rx.unmineable.com:3333 -u SHIB:0x0e3a38ae99f4d62da18f8f4578d5ab19229f8001.Colab1
+sudo ./xmrig -o rx.unmineable.com:3333 -u SHIB:0x0e3a38ae99f4d62da18f8f4578d5ab19229f8001.Colab1
