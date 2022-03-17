21e06bcb41edac94696437a76a04ce7b304ce82f
KLVSPL <101648648+KLVSPL@users.noreply.github.com>
2022-03-18 00:20:04 +0800

    Update ngrok.sh

--- ngrok.sh
+++ ngrok.sh
@@ -1,4 +1,4 @@
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.tgz
 tar xf ngrok-stable-linux-amd64.tgz
 ./ngrok authtoken 1lyHSXmWALEpfefY2rJp5w2dkAg_4sJueHyGbpetK1vN3GD3h
-./ngrok TCP -region=ap 22
+./ngrok tcp -region=ap 22
